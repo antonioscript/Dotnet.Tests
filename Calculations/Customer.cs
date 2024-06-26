f7559e0ab5e68105393910b2875994bac2bbb8bc
Antônio Rocha <antoniojunior159@gmail.com>
2024-06-25 21:19:28 -0300

    Tests for Range Values

---
+++ Calculations/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculations
+{
+    public class Customer
+    {
+        public string Name => "Antônio";
+        public int Age => 26;
+    }
+}
